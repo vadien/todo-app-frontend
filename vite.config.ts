1eda4d1dfa18685b57a43816b15913819122aa6b
vadien <david.c.neill@gmail.com>
2024-09-11 22:02:25 +1000

    Fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://vadien.github.io/todo-app-frontend/",
+  base: "/todo-app-frontend/",
 });
